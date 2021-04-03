53cf21a8bc8b4ca0521f12a98cfd1801e1d90ad8
Andrew Whang <56315025+akwhang@users.noreply.github.com>
2021-04-02 19:59:59 -0500

    Add files via upload
    Script for finding regex pattern in files in directory

---
+++ filesearch2.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+user_input = input('What is the name of your directory')
+directory = os.listdir(user_input)
+
+pattern = re.compile(".*50870.*SIGP.*")
+
+for fname in directory:
+    if os.path.isfile(user_input + os.sep + fname):
+        # Full path
+        f = open(user_input + os.sep + fname, 'r')
+
+        if re.search(pattern, f.read()):
+            print('found string in file %s' % fname)
+        f.close()
