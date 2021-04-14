d0bae07a826760e4fc9601895da00c3362c7f859
Andrew Whang <56315025+akwhang@users.noreply.github.com>
2021-04-14 18:34:03 -0500

    Add files via upload
    Now script will write lines to file instead of printing to console terminal.

--- filesearch.py
+++ filesearch.py
@@ -1,17 +1,21 @@
 import os
 import re
 
-user_input = input('What is the name of your directory')
+user_input = input('What is the name of your directory?')
 directory = os.listdir(user_input)
 
 pattern = re.compile(".*50870.*SIGP.*")
-
+results = []
 for fname in directory:
     if os.path.isfile(user_input + os.sep + fname):
         with open(user_input + os.sep + fname, 'r') as f:
             for line in f:
                 if re.search(pattern, line):
-                    print('%s' % line)
+                    results.append(line)
                     # To print filename with string, use below:
                     # print('found string in file %s' % fname)
         f.close()
+print(results)
+with open('results.txt', 'w') as  final:
+    for line in results:
+        final.write('%s' % line)
